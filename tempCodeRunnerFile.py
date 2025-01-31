3f924a881c52a3d52e420a9889764b8d572dc2fd
doniyoryodgorov <doniyor.yodgorov1995@mail.ru>
2025-01-31 16:40:04 +0500

    sql_homework

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(sum_ab)
+print(product_ab)
+print(square_a)
+print(square_b)
