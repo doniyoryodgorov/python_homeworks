a09c982da1b57b95693ba7dd03c283765fad6c91
DONIYOR YODGOROV <doniyor.yodgorov1995@mail.ru>
2025-02-22 21:35:00 +0500

    Create strings.py
    lesson_1_homework

---
+++ lesson-1/homework/strings.py
@@ -0,0 +1,24 @@
+#Task_1
+side = int(input("kvadratning tominini kiriting: "))
+p = 4 * side
+c = side ** 2
+print("kvadratning perimetri", p, "ga teng", "uning yuzi esa", c, "ga teng")
+#Task_2
+diameter=int(input("aylana diametrini kiriting: ")) 
+print("aylana uzunligi", 3.14*diameter, "ga teng")
+#Task_3
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+mean = (a + b) / 2
+print("Ikki sonning o'rtacha qiymati", mean, "ga teng")
+#Task_4
+a = float(input("Birinchi sonni kiriting: "))
+b = float(input("Ikkinchi sonni kiriting: "))
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+print(sum_ab)
+print(product_ab)
+print(square_a)
+print(square_b)
